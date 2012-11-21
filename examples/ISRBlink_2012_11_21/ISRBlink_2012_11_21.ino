7a1813df382548ff8eae826f395de55ab1d53af3
Matt Karau <mkarau@gmail.com>
2012-11-21 15:09:53 +0100

    2012-11-21 Temp Sense

--- examples/ISRBlink_2012_11_21/ISRBlink_2012_11_21.ino
+++ examples/ISRBlink_2012_11_21/ISRBlink_2012_11_21.ino
@@ -8,20 +8,29 @@ long TimerOneMicros = 1200;
 long PrintTimer = 0;
 //long PrintIntervalMillis = 100;
 long PrintIntervalMicros = 100000;
- 
+
+int ledPin = 13;
+
+int tempSensePin = A1;
+int virtualGroundPin = A0;
+
+int temperature = 0;
+long tempSensingTimer = 0;
+long tempSensingIntervalMicros = 500000;
+
 void setup() 
 {
   // Use A0 as virtual GND
-  pinMode(A0, OUTPUT);
-  digitalWrite(A0, LOW);
+  pinMode(virtualGroundPin, OUTPUT);
+  digitalWrite(virtualGroundPin, LOW);
   
   // A1 is our sensing pin.  We need to have a pull-up to create voltage-divider.
-  pinMode(A1, INPUT);
-  digitalWrite(A1, HIGH);
+  pinMode(tempSensePin, INPUT);
+  digitalWrite(tempSensePin, HIGH);
   
   // Initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards
-  pinMode(13, OUTPUT);    
+  pinMode(ledPin, OUTPUT);    
   
   Timer1.initialize(TimerOneMicros); // set a timer of length 100000 microseconds (or 0.1 sec - or 10Hz => the led will blink 5 times, 5 cycles of on-and-off, per second)
   Timer1.attachInterrupt( blinky ); // attach the service routine here
@@ -36,17 +45,24 @@ void loop()
     stateChanged = false;
   }
 */  
+  if (tempSensingTimer >= tempSensingIntervalMicros) {
+    tempSensingTimer = 0;
+    temperature = analogRead(tempSensePin);
+  }
+  
   if (LEDBlinkTimer >= LEDBlinkIntervalMicros) {
-    Serial.println(LEDBlinkTimer);
+//    Serial.println(LEDBlinkTimer);
     LEDBlinkTimer = 0;
-    int state = digitalRead(13); 
+    int state = digitalRead(ledPin); 
 //    delay(300);
-    digitalWrite( 13, !state );
+    digitalWrite( ledPin, !state );
   }
   
   if (PrintTimer >= PrintIntervalMicros) {
     PrintTimer = 0;
-    Serial.println("Alive");
+    Serial.print("Alive.\t");
+    Serial.print("Temp: ");
+    Serial.println(temperature);
   }
   
   // Main code loop
@@ -61,5 +77,6 @@ void blinky()
 //  stateChanged = true;
   LEDBlinkTimer = LEDBlinkTimer + TimerOneMicros;
   PrintTimer += TimerOneMicros;
+  tempSensingTimer += TimerOneMicros;
   // Toggle LED
 }
